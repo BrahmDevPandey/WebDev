ceda51c0c9f853e39402fd337ced99101265ab8e
Brahm Dev Pandey <brahmdevpandey762@gmail.com>
2023-02-25 15:23:43 +0530

    refreshing

--- css/tailwind/tailwind-tut/src/App.js
+++ css/tailwind/tailwind-tut/src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <div>Body</div>;
+  return (
+    <div className="text-lg text-center font-bold m-4">
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className="bg-sky-500 p-5 rounded-md">First col</div>
+        <div className="bg-sky-500 p-5 rounded-md">Second col</div>
+        <div className="bg-sky-500 col-span-2 p-5 rounded-md">Third col</div>
+        <div className="bg-sky-500 p-5 rounded-md">Fourth col</div>
+        <div className="bg-sky-500 p-5 col-span-2 rounded-md">Fifth col</div>
+        <div className="bg-sky-500 p-5 rounded-md">Sixth col</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
